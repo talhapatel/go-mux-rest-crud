524465a2eeb96067b970fa09e11dd75fe5ac973c
Talha Patel <talhapatel.tp@gmail.com>
2021-04-16 14:34:51 +0530

    add mux and make get all api

--- rest-api/main.go
+++ rest-api/main.go
@@ -4,19 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
+    "github.com/gorilla/mux"
 )
 
 //  create struct same like ES6 / java class
+
+type Artical struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Artical
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
-
+func returnAllArticles(w http.ResponseWriter , r *http.Request){
+	fmt.Println("Endpoint Hit: returnAllAricles")
+	json.newEndcoder(w).Encode(Articles)
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+    // creates a new instance of a mux router
+    myRouter := mux.NewRouter().StrictSlash(true)
+    // replace http.HandleFunc with myRouter.HandleFunc
+    myRouter.HandleFunc("/", homePage)
+    myRouter.HandleFunc("/all", returnAllArticles)
+    // finally, instead of passing in nil, we want
+    // to pass in our newly created router as the second
+    // argument
+    log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	Articles = []Article{
+        Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+    }
 	handleRequests()
 }
